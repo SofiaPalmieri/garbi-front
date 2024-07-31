336e784f017ced401e745b9672ef4e757a3ed331
SofiaPalmieri <spalmieri@frba.utn.edu.ar>
2024-07-31 10:50:24 -0300

    Added filters on container page

--- src/pages/Containers/ContainerPage.jsx
+++ src/pages/Containers/ContainerPage.jsx
@@ -1,15 +1,22 @@
 import {
-  FilterSideComponent 
+  FilterSideComponent
 } from '../../components/FilterSideComponent';
 import {
   ContainerContent 
 } from './ContainerContent';
+import ContainerFilters from './ContainerFilters';
+
 
 export const ContainerPage = () => {
+
+
   return (
     <FilterSideComponent
       prefix={'Gestión'}
       title={'Contenedores'}
+      renderFilters={
+        <ContainerFilters />
+      }
       component={() => <ContainerContent />}
     />
   );
