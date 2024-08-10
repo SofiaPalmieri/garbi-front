9576241d503d32e2156dd8a37f51a89db477d807
nacho <nachoengineeringdev@gmail.com>
2024-08-10 10:03:59 -0300

    fix header

--- src/pages/ReportDetails/ReportDetailsDescriptionHeader.jsx
+++ src/pages/ReportDetails/ReportDetailsDescriptionHeader.jsx
@@ -1,43 +1,42 @@
 import {
-  Typography, Box, Chip 
+  Typography, Box, Chip
 } from '@mui/material';
 
 export const ReportDetailsDescriptionHeader = ({
-  title, state 
+  title, state
 }) => {
   return (
-    <Box>
-      <Box
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%'
+      }}
+    >
+      <Typography
         sx={{
-          display: 'flex',
-          alignItems: 'center', 
-          height:'100%',
-          width:'100%'
+          fontFamily: 'Roboto',
+          marginLeft: '24px',
+          fontSize: '24px',
+          fontWeight: '500',
+          lineHeight: '39.84px',
+          letterSpacing: '0.4px',
+          textAlign: 'left'
         }}
       >
-        <Typography
-          sx={{
-            fontFamily: 'Roboto',
-            fontSize: '24px',
-            fontWeight: '500',
-            lineHeight: '39.84px',
-            letterSpacing: '0.4px',
-            textAlign: 'left'
-          }}
-        >
-          {title}
-        </Typography>
-        <Chip
-          label={state}
-          color='default'
-          size='small'
-          variant='outlined'
-          sx={{
-            marginLeft: '20px', 
-            padding: '3px 4px',
-          }}
-        />
-      </Box>
+        {title}
+      </Typography>
+      <Chip
+        label={state}
+        color='default'
+        size='small'
+        variant='outlined'
+        sx={{
+          marginLeft: '20px',
+          padding: '3px 4px',
+        }}
+      />
     </Box>
   );
 };
