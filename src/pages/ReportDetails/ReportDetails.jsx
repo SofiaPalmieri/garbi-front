6f044eff033032dc8a52ad1534a1f074c37aecc7
SofiaPalmieri <spalmieri@frba.utn.edu.ar>
2024-08-12 17:36:03 -0300

    Fixes

--- src/pages/ReportDetails/ReportDetails.jsx
+++ src/pages/ReportDetails/ReportDetails.jsx
@@ -1,14 +1,10 @@
 import {
-  Box 
-} from '@mui/system';
-import {
-  Button, Paper 
+  Box, Paper, Button 
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {
   ReportDetailsSideComponent 
 } from '../../components/ReportDetailsSideComponent/ReportDetailsSideComponent';
-
 
 const colors = {
   NEW: '#EF6C0080',
@@ -27,17 +23,8 @@ const getColor = (state) => {
 };
 
 export const ReportDetails = ({
-  report 
+  content, state 
 }) => {
-  const {
-    state,
-    date,
-    reportedBy,
-    neighborhood,
-    address,
-    containerID
-  } = report;
-
   return (
     <Box
       sx={{
@@ -59,7 +46,7 @@ export const ReportDetails = ({
         {state}
         <ExpandMoreIcon
           sx={{
-            color: getColor(state) 
+            color: getColor(state),
           }}
         />
       </Button>
@@ -67,16 +54,24 @@ export const ReportDetails = ({
       <Paper
         sx={{
           width: '100%',
-          height: '394px',
           gap: '48px',
           border: '1px solid',
           marginTop: '10px',
           borderColor: '#0000001F',
         }}
       >
-        <ReportDetailsSideComponent
-          report={report}
-        />
+        {content.map((item, index) => (
+          <ReportDetailsSideComponent
+            key={index}
+            icon={item.icon}
+            title={item.title}
+            description={item.description}
+            titleIcon={item.titleIcon}
+            description2={item.description2}
+            button={item.button}
+            buttonIcon={item.buttonIcon}
+          />
+        ))}
       </Paper>
     </Box>
   );
--- src/pages/ReportDetails/ReportDetailsPage.jsx
+++ src/pages/ReportDetails/ReportDetailsPage.jsx
@@ -1,20 +1,71 @@
 import {
   Box 
 } from '@mui/system';
-
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
+import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import {
   ReportDetails 
-} from './ReportDetails';
+} from '../ReportDetails/ReportDetails';
 
 export const ReportDetailsPage = ({
-  report = {
-    state: 'NUEVO',
-    date: '19/02/2024 - 09.20 hs',
-    reportedBy: 'Ciudadano',
-    neighborhood: 'Villa del Parque - Área 2',
-    address: 'Av. Honorio Pueyrredón 1234',
-    containerID: '#123456'
-  }
+  content = [
+    {
+      icon: <CalendarMonthIcon
+        sx={{
+          color: '#2121213B' 
+        }}
+      />,
+      title: 'Fecha de creación',
+      description: '19/02/2024 - 09.20 hs'
+    },
+    {
+      icon: <PersonOutlineOutlinedIcon
+        sx={{
+          color: '#2121213B' 
+        }}
+      />,
+      title: 'Reportado por',
+      description: 'Ciudadano'
+    },
+    {
+      icon: <RoomOutlinedIcon
+        sx={{
+          color: '#2121213B' 
+        }}
+      />,
+      titleIcon: <OpenInNewOutlinedIcon
+        sx={{
+          width: '20px',
+          height: '20px',
+          marginLeft: '5px' 
+        }}
+      />,
+      title: 'Ubicación',
+      description: 'Villa del Parque - Área 2',
+      description2: 'Av. Honorio Pueyrredón 1234'
+    },
+    {
+      icon: <DeleteOutlineOutlinedIcon
+        sx={{
+          color: '#2121213B' 
+        }}
+      />,
+      title: 'Contenedor',
+      description: '#123456',
+      button: 'HISTORIAL',
+      buttonIcon: <OpenInNewOutlinedIcon
+        sx={{
+          width: '20px',
+          height: '20px',
+          marginLeft: '5px' 
+        }}
+      />
+    }
+  ],
+  state = 'NUEVO'
 }) => {
   return (
     <Box
@@ -56,7 +107,7 @@ export const ReportDetailsPage = ({
             }}
           >
             <Box
-              sx ={{
+              sx={{
                 height: 1,
                 flex: 1,
                 backgroundColor: 'blue',
@@ -66,20 +117,20 @@ export const ReportDetailsPage = ({
               }}
             >
               <Box
-                sx = {{
+                sx={{
                   backgroundColor: 'orange',
                   height: '4rem',
                 }}
               />
               <Box
-                sx  ={{
+                sx={{
                   backgroundColor: 'red',
                   height: '21.875rem',
                   mb: '1.5rem'
                 }}
               />
               <Box
-                sx ={{
+                sx={{
                   flex: 1,
                   backgroundColor: 'white',
                 }}
@@ -93,9 +144,9 @@ export const ReportDetailsPage = ({
               }}
             >
               <ReportDetails
-                report={report}
+                content={content}  // Pasa el array de objetos tal cual está
+                state={state}      // Pasa el estado si es necesario
               />
-              {/* {code here....} */}
             </Box>
           </Box>
           <Box>
