ac4a494294df7e4e466c8d4df474a4032c95a6eb
SofiaPalmieri <spalmieri@frba.utn.edu.ar>
2024-08-13 15:22:37 -0300

    Added report details title

---
+++ src/pages/ReportDetails/ReportDetailsTitle.jsx
@@ -0,0 +1,35 @@
+import {
+  Typography, Box 
+} from '@mui/material';
+
+const navigationTitle = 'Reportes > Reporte';
+
+export const ReportDetailsTitle = ({
+  title 
+}) => {
+  return (
+    <Box
+      sx={{
+        width: '1,440px',
+        height: '42px',
+        padding: '0px 64px 0px 64px',
+      }}
+    >
+      <Typography
+        sx={{
+          width: '1288px',
+          height: '42px',
+          fontFamily: 'Roboto',
+          fontSize: '34px',
+          fontWeight: '400',
+          lineHeight: '41.99px',
+          letterSpacing: '0.25px',
+          textAlign: 'left',
+          marginTop: '20px'
+        }}
+      >
+        {navigationTitle} {title}
+      </Typography>
+    </Box>
+  );
+};
