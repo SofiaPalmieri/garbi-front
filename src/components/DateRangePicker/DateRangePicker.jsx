d7e67bf31eed1b061b8067fbc40b6cf4168b772f
SantiagoOrlando29 <sorlando@frba.utn.edu.ar>
2024-09-08 22:40:29 -0300

    add date range picker

---
+++ src/components/DateRangePicker/DateRangePicker.jsx
@@ -0,0 +1,60 @@
+import {
+  TextField
+} from '@mui/material';
+import {
+  forwardRef,
+  useState
+} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './DateRangePicker.css';
+import {
+  es 
+} from 'date-fns/locale'; 
+import {
+  subDays 
+} from 'date-fns'
+
+export const DateRangePicker = () => {
+  const defaultStartDate = subDays(new Date(), 7);
+  const defaultEndDate = subDays(new Date(), 1);
+  const [dateRange, setDateRange] = useState([defaultStartDate, defaultEndDate]);
+  const [startDate, endDate] = dateRange;
+
+  const CustomInput = forwardRef(
+    ({
+      value, onClick 
+    }, ref) => (
+      <TextField
+        id='date-picker'
+        value={value}
+        onClick={onClick}
+        variant='outlined'
+        ref={ref}
+        sx={{
+          mt: 1,
+          width: '224px' 
+        }}
+        size='small'
+      />
+    ),
+  );
+
+  CustomInput.displayName = 'CustomInput';
+
+  return (
+    <DatePicker
+      selectsRange={true}
+      startDate={startDate}
+      endDate={endDate}
+      onChange={(update) => {
+        setDateRange(update);
+      }}
+      locale={es}
+      customInput={<CustomInput/>}
+      dateFormat='dd/MM/yyyy'
+      maxDate={new Date()}
+      calendarStartDay={7}
+    />
+  );
+};
